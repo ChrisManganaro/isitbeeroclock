d0062922d77a7764d03c22691ecb53b81fd61945
Chris Manganaro <chris.manganaro@isobar.com>
2018-10-12 15:26:37 +0800

    tidy for friday launch

--- src/beer/index.js
+++ src/beer/index.js
@@ -37,6 +37,39 @@ const Pyro = css`
 `;
 
 class Beer extends Component {
+  state = {
+    beerTime: false,
+    timeLeft: false
+  };
+
+  beerTime = {
+    day: 5,
+    hour: 16,
+    mins: 0
+  };
+
+  timeout = null;
+
+  componentDidMount() {
+    this.isItTime();
+    this.timeout = setInterval(this.isItTime, 1 * 1000);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeout);
+  }
+
+  isItTime = () => {
+    const date = new Date();
+
+    this.setState({
+      beerTime:
+        date.getDay() === this.beerTime.day &&
+        date.getHours() >= this.beerTime.hour &&
+        date.getMinutes() >= this.beerTime.mins
+    });
+  };
+
   fireWorks = () => {
     return (
       <div className={`pyro ${Pyro}`}>
@@ -46,57 +79,13 @@ class Beer extends Component {
     );
   };
 
-  isItTime = () => {
-    const date = new Date();
-
-    if (date.getDay() === 5 && date.getHours() >= 16) {
-      return <div>Yes {this.fireWorks()}</div>;
-    }
-    return 'No';
-  };
-
-  getNextDayOfWeek = (date, dayOfWeek, hour) => {
-    var resultDate = new Date(date.getTime());
-    resultDate.setDate(date.getDate() + ((7 + dayOfWeek - date.getDay()) % 7));
-    resultDate.setHours(hour, 0, 0, 0);
-    return resultDate;
-  };
-
-  countdown = countDownDate => {
-    const x = setInterval(() => {
-      // Get todays date and time
-      const now = new Date().getTime();
-
-      // Find the distance between now an the count down date
-      const distance = countDownDate - now;
-
-      // Time calculations for days, hours, minutes and seconds
-      const days = Math.floor(distance / (1000 * 60 * 60 * 24)).toString();
-      const hours = Math.floor(
-        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      ).toString();
-      const minutes = Math.floor(
-        (distance % (1000 * 60 * 60)) / (1000 * 60)
-      ).toString();
-      const seconds = Math.floor((distance % (1000 * 60)) / 1000).toString();
-
-      return `${days} Days, ${hours} Hours, ${minutes} Minutes, ${seconds}`;
-    }, 1000);
-  };
-  timeLeft = () => {
-    if (this.isItTime() === 'No') {
-      let countDownDate = this.getNextDayOfWeek(new Date(), 5, 16);
-      // Update the count down every 1 second
-      this.countdown(countDownDate);
-    }
-  };
-
   render() {
     return (
       <Container>
         <Question>Is it beer O'Clock?</Question>
-        <Answer>{this.isItTime()}</Answer>
-        <TimeLeft>{this.timeLeft()}</TimeLeft>
+        <Answer>
+          {this.state.beerTime ? <div>Yes {this.fireWorks()}</div> : 'No'}
+        </Answer>
       </Container>
     );
   }
